a7642d551866d2929458b5172f71d4d2432e5e6d
Sebastian_Milian <milianmejiaedgarsebastian@yahoo.com>
2021-10-01 13:42:05 -0600

    Cifrado Cesar: llenando abecedario y eliminando repetidos en clave

---
+++ Libreria_ED2/CifradorCesar.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Libreria_ED2
+{
+    public class CifradorCesar
+    {
+        int longitudBuffer;
+
+        public CifradorCesar(int _longitudBuffer)
+        {
+            longitudBuffer = _longitudBuffer;
+        }
+
+        public void Cifrar(string dirLectura, string dirEscritura, string clave)
+        {
+
+            char[] letrasClave = clave.ToCharArray();
+            Dictionary<char, byte> eliminarRepetidosClave = new Dictionary<char, byte>();
+            Dictionary<byte, byte> abcModificado = new Dictionary<byte, byte>();
+            LinkedList<byte> abcModPrevio = new LinkedList<byte>();
+            byte iterador = 0;
+            int cantLeida = 0;
+
+            for (int i = 0; i < letrasClave.Length; i++)
+            {
+                if (eliminarRepetidosClave.ContainsKey(letrasClave[i]) == false)
+                {
+                    eliminarRepetidosClave.Add(letrasClave[i], Convert.ToByte(letrasClave[i]));
+                }
+            }
+
+            for (int i = 0; i < 256; i++)
+            {
+                abcModPrevio.AddLast(Convert.ToByte(i));
+            }
+
+        }
+    }
+}
---
+++ Pruebas_Consola/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pruebas_Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
