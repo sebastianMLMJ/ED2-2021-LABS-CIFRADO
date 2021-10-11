ad9f094412cdeb5184470bc6aeb68793f46f3c2f
Sebastian_Milian <milianmejiaedgarsebastian@yahoo.com>
2021-10-11 13:34:18 -0600

    segunda ronda y permutacion inversa

--- Pruebas_Consola/Program.cs
+++ Pruebas_Consola/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Libreria_ED2;
+using System.IO;
 namespace Pruebas_Consola
 {
     class Program
@@ -7,8 +8,13 @@ namespace Pruebas_Consola
         static void Main(string[] args)
         {
             CifradorSDES tester = new CifradorSDES(1024);
-
-            tester.Cifrar("C:\\ABF\\cuento.txt", "C:\\ABF", "C:\\ABF\\Permutaciones.txt", "ejemplo.txt",364);
+            int sem = 211;
+            byte prueba = Convert.ToByte(sem);
+            BinaryWriter bw = new BinaryWriter(new FileStream("C:\\ABF\\ejemplosdes.txt",FileMode.OpenOrCreate));
+            bw.Write(prueba);
+            bw.Close();
+            
+            tester.Cifrar("C:\\ABF\\ejemplosdes.txt", "C:\\ABF", "C:\\ABF\\Permutaciones.txt", "ejemplo.txt",364);
             
 
         }
